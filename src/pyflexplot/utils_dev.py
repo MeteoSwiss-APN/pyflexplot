fab07c5730b496502c79a984e8900e0f6491d761
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2019-05-24 16:43:57 +0200

    refine ipython: enable colors and vi mode

--- src/pyflexplot/utils_dev.py
+++ src/pyflexplot/utils_dev.py
@@ -2,12 +2,15 @@
 """
 Utils for development.
 """
+import logging as log
 import sys
 import warnings
 import IPython
 
 
-def ipython(_globals_, _locals_, _msg_=None, _err_=66):
+def ipython(
+        _globals_, _locals_, _msg_=None, _err_=66, _log_lvl_=log.INFO,
+        _colors_=True, _bg_='light', _edit_mode_='vi'):
     """Drop into an iPython shell with all global and local variables.
 
     To pass the global and local variables, call with globals() and locals()
@@ -25,15 +28,30 @@ def ipython(_globals_, _locals_, _msg_=None, _err_=66):
     >>> ipython(globals(), locals(), "I'm here!")
 
     """
+    log.getLogger().setLevel(_log_lvl_)
+
+    _kwargs_ = {'display_banner': None, 'editing_mode': _edit_mode_}
+
+    if _colors_:
+        if _bg_ == 'light':
+            _kwargs_['colors'] = 'LightBG'
+        elif _bg_ == 'dark':
+            _kwargs_['colors'] = 'Linux'
+        else:
+            _kwargs_['colors'] = _colors_
+
+    globals().update(_globals_)
+    locals().update(_locals_)
+
+    print('\n----------\n')
+    if _msg_ is not None:
+        print("\n{l}\n{m}\n{l}\n".format(l="*" * 60, m=_msg_))
+
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
         import IPython
-        print('\n----------\n')
-        globals().update(_globals_)
-        locals().update(_locals_)
-        if _msg_ is not None:
-            print("\n{l}\n{m}\n{l}\n".format(l="*" * 60, m=_msg_))
-        IPython.embed(display_banner=None)
-        if _err_ is not None:
-            sys.exit(_err_)
+        IPython.embed(**_kwargs_)
+
+    if _err_ is not None:
+        sys.exit(_err_)
