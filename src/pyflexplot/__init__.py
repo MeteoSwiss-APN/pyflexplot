b62ebe595149760b9960d1dc52c7956bd5660285
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2019-06-03 10:02:40 +0200

    add cartopy offline data (coastlines)

--- src/pyflexplot/__init__.py
+++ src/pyflexplot/__init__.py
@@ -4,3 +4,11 @@
 __author__ = """Stefan Ruedisuehli"""
 __email__ = 'stefan.ruedisuehli@env.ethz.ch'
 __version__ = '0.1.0'
+
+import os.path
+import cartopy
+
+# Point cartopy to storerd offline data
+dir = os.path.dirname(os.path.abspath(__file__))
+cartopy_dir = os.path.abspath(f'{dir}/../../cartopy_data')
+cartopy.config['pre_existing_data_dir'] = cartopy_dir
