9cde6257df72c2838e1b27faf94ddec69cbf518b
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2019-10-21 16:11:29 +0200

    work on Words class and tests

--- src/words/words.py
+++ src/words/words.py
@@ -28,28 +28,48 @@ class Words:
 
         """
         self.name_ = name_
-        self.default_ = default_
 
-        self.langs_ = None
+        langs = None
         self._words = {}
         for name, word in words.items():
             try:
-                langs = list(word.keys())
+                langs_i = list(word.keys())
             except AttributeError:
                 raise ValueError(f"word {name} not a dict: {word}")
-            if self.langs_ is None:
-                self.langs_ = langs
-            elif set(langs) != set(self.langs_):
+            if langs is None:
+                langs = langs_i
+            elif set(langs) != set(langs):
                 raise ValueError(
                     f"word '{name}' not defined in all necessary languages: "
-                    f"set({langs}) != set({self.langs_})")
-            if self.default_ is None:
-                self.default_ = next(iter(self.langs_))
-            elif self.default_ not in self.langs_:
+                    f"set({langs}) != set({langs})")
+            if default_ is None:
+                default_ = next(iter(langs))
+            elif default_ not in langs:
                 raise ValueError(
-                    f"invalid default language '{self.default_}': "
-                    f"not in {self.langs_}")
-            self._words[name] = Word(name, default=self.default_, **word)
+                    f"invalid default language '{default_}': "
+                    f"not in {langs}")
+            self._words[name] = Word(name, default=default_, **word)
+        self._langs_ = langs
+        self.set_default(default_)
+
+    def set_default(self, lang):
+        """Change default language recursively for all words."""
+        if lang not in self.langs_:
+            raise ValueError(
+                f"unknown language '{lang}': not among {self.langs_}")
+        self._default_ = lang
+        for word in self._words.values():
+            word.set_default(lang)
+
+    @property
+    def default_(self):
+        """Return the default language."""
+        return self._default_
+
+    @property
+    def langs_(self):
+        """Return the languages the words are defined in."""
+        return [lang for lang in self._langs_]
 
     def __repr__(self):
         s_name = f' ({self.name})' if self.name else ''
--- tests/words/test_words.py
+++ tests/words/test_words.py
@@ -27,3 +27,39 @@ class Test_Basic:
     def test_langs(self):
         assert str(self.ws.train.de) == 'Zug'
         assert str(self.ws.high_school.de) == 'Mittelschule'
+
+
+class Test_Complex:
+    """Test all functionality for a more complex set of words."""
+
+    ws = Words(
+        default_='de',
+        train={
+            'en': 'train',
+            'de': 'Zug',
+        },
+        high_school={
+            'en': 'high school',
+            'de': 'Mittelschule',
+        },
+        at={
+            'en': 'at',
+            'de': {
+                'place': 'bei',
+                'time': 'um',
+                'level': 'auf',
+            },
+        },
+    )
+
+    def test_default(self):
+        assert str(self.ws.train) == 'Zug'
+        assert str(self.ws.high_school) == 'Mittelschule'
+        assert str(self.ws.at) == 'bei'
+
+    def test_change_default(self):
+        assert self.ws.default_ == 'de'
+        assert str(self.ws.train) == 'Zug'
+        self.ws.set_default('en')
+        assert self.ws.default_ == 'en'
+        assert str(self.ws.train) == 'train'
