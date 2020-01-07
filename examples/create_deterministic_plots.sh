e50cc8781e232f63fa14740228bebb6dd63248b5
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-01-07 17:37:16 +0100

    update paths in example plot script and introduce some variables

--- examples/create_deterministic_plots.sh
+++ examples/create_deterministic_plots.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 data="/scratch/ruestefa/flexpart_visualization/test/data"
+infile="${data}/cosmo-1_2019052800.nc"
 
 dest="./png"
 mkdir -pv "${dest}"
 
-pyflexplot --lang=en deterministic concentration --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --level-ind=0 --time-ind=3 --no-integrate
-pyflexplot --lang=de deterministic concentration --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --level-ind=0 --time-ind=3 --no-integrate
-pyflexplot --lang=en deterministic concentration --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --level-ind=0 --time-ind=3 --no-integrate
-pyflexplot --lang=de deterministic concentration --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --level-ind=0 --time-ind=3 --no-integrate
+outfile_fmt="${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png"
 
-pyflexplot --lang=en deterministic concentration --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=1 --level-ind=0 --time-ind=10 --integrate
-pyflexplot --lang=de deterministic concentration --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=1 --level-ind=0 --time-ind=10 --integrate
-pyflexplot --lang=en deterministic concentration --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=1 --level-ind=0 --time-ind=10 --integrate
-pyflexplot --lang=de deterministic concentration --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_level-{level_ind}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=1 --level-ind=0 --time-ind=10 --integrate
+for lang in en de; do
+for domain in auto ch; do
 
-pyflexplot --lang=en deterministic deposition --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --deposition-type=tot --time-ind=3 --integrate
-pyflexplot --lang=de deterministic deposition --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --deposition-type=tot --time-ind=3 --integrate
-pyflexplot --lang=en deterministic deposition --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --deposition-type=tot --time-ind=3 --integrate
-pyflexplot --lang=de deterministic deposition --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --species-id=2 --deposition-type=tot --time-ind=3 --integrate
+pyflexplot --lang=${lang} deterministic concentration --domain=${domain} -i "${infile}" -o "${outfile_fmt}" --species-id=2 --level-ind=0 --time-ind=3 --no-integrate
 
-pyflexplot --lang=en deterministic deposition --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --plot-var=affected_area_mono --species-id=1+2 --deposition-type=tot --time-ind=10 --integrate
-pyflexplot --lang=de deterministic deposition --domain=auto -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --plot-var=affected_area_mono --species-id=1+2 --deposition-type=tot --time-ind=10 --integrate
-pyflexplot --lang=en deterministic deposition --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --plot-var=affected_area_mono --species-id=1+2 --deposition-type=tot --time-ind=10 --integrate
-pyflexplot --lang=de deterministic deposition --domain=ch -i "${data}/test_case_2.nc" -o "${dest}/test_case1_{variable}_species-{species_id}_time-{time_ind:02d}_domain-{domain}_{lang}.png" --plot-var=affected_area_mono --species-id=1+2 --deposition-type=tot --time-ind=10 --integrate
+pyflexplot --lang=${lang} deterministic concentration --domain=${domain} -i "${infile}" -o "${outfile_fmt}" --species-id=1 --level-ind=0 --time-ind=10 --integrate
+
+pyflexplot --lang=${lang} deterministic deposition --domain=${domain} -i "${infile}" -o "${outfile_fmt}" --species-id=2 --deposition-type=tot --time-ind=3 --integrate
+
+pyflexplot --lang=${lang} deterministic deposition --domain=${domain} -i "${infile}" -o "${outfile_fmt}" --plot-var=affected_area_mono --species-id=1+2 --deposition-type=tot --time-ind=10 --integrate
+
+done
+done
