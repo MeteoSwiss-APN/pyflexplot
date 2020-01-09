c8ec62f998330c618f332a17fff93f59d18be631
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-01-09 10:18:16 +0100

    add proper error messages if cartopy import fails because libproj or libgeos is missing
    add dependency click-config-file
    
    clean up and update requirements files

--- src/pyflexplot/__init__.py
+++ src/pyflexplot/__init__.py
@@ -5,11 +5,22 @@ __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
 __version__ = "0.1.1"
 
+import sys
 import os.path
 
-# Point cartopy to storerd offline data
-import cartopy
+try:
+    import cartopy
+except ImportError as e:
+    if "libproj" in str(e):
+        msg = f"cannot import 'cartopy': external dependency 'proj' missing ({e})"
+    if "libgeos" in str(e):
+        msg = f"cannot import 'cartopy': expernal dependency 'geos' missing ({e})"
+    else:
+        raise
+    print(msg, file=sys.stderr)
+    sys.exit(1)
 
+# Point cartopy to storerd offline data
 dir = os.path.dirname(os.path.abspath(__file__))
 cartopy_dir = os.path.abspath(f"{dir}/cartopy_data")
 cartopy.config["pre_existing_data_dir"] = cartopy_dir
