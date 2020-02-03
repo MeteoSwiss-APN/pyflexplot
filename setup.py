d4368f6bb4afd2afa0802b1a6ca9a5bae1ac183b
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-02-03 12:17:38 +0100

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ description_files = ["README.rst", "HISTORY.rst"]
 
 metadata = {
     "name": "pyflexplot",
-    "version": "0.4.1",
+    "version": "0.4.2",
     "description": "PyFlexPlot visualizes FLEXPART particle dispersion simulations.",
     "long_description": "\n\n".join([read_file(f) for f in description_files]),
     "author": "Stefan Ruedisuehli",
