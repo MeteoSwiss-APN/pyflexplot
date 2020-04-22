979b1e46f4965bac98bce0d8f9c751fab86ad8f0
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-04-22 18:28:47 +0200

    add additional ensemble plot tests and clean up existing plot tests

--- tests/slow/pyflexplot/test_plots/test_cosmo2_ensemble.py
+++ tests/slow/pyflexplot/test_plots/test_cosmo2_ensemble.py
@@ -2,6 +2,9 @@
 """
 Test the elements of complete plots based on ensemble COSMO-2 data.
 """
+# Third-party
+import pytest
+
 # Local
 from .shared import _CreateReference  # noqa:F401
 from .shared import _TestBase
@@ -19,18 +22,12 @@ class Test_EnsMedian_Concentration(_TestBase):
         "outfile": "dummy.png",
         "plot_type": "ens_median",
         "variable": "concentration",
-        "deposition_type": "none",
         "integrate": False,
         "combine_species": True,
         "simulation_type": "ensemble",
         "ens_member_id": ENS_MEMBER_IDS,
-        "ens_param_mem_min": None,
-        "ens_param_thr": None,
         "lang": "de",
         "domain": "auto",
-        "nageclass": None,
-        "noutrel": None,
-        "numpoint": None,
         "species_id": None,
         "time": (5,),
         "level": (0,),
@@ -45,19 +42,76 @@ class Test_EnsMax_IntegratedConcentration(_TestBase):
         "outfile": "plot.png",
         "plot_type": "ens_max",
         "variable": "concentration",
-        "deposition_type": "none",
         "integrate": True,
         "combine_species": True,
         "simulation_type": "ensemble",
         "ens_member_id": ENS_MEMBER_IDS,
-        "ens_param_mem_min": None,
-        "ens_param_thr": None,
         "lang": "de",
         "domain": "ch",
-        "nageclass": None,
-        "noutrel": None,
-        "numpoint": None,
         "species_id": None,
         "time": (10,),
+        "level": (0,),
+    }
+
+
+# class Test_EnsMean_TotalDeposition(_CreateReference):
+class Test_EnsMean_TotalDeposition(_TestBase):
+    reference = "ref_cosmo2e_ens_mean_total_deposition"
+    setup_dct = {
+        "infile": INFILE_NAME,
+        "outfile": "plot.png",
+        "plot_type": "ens_mean",
+        "variable": "deposition",
+        "deposition_type": "tot",
+        "integrate": True,
+        "combine_species": True,
+        "simulation_type": "ensemble",
+        "ens_member_id": ENS_MEMBER_IDS,
+        "lang": "de",
+        "domain": "auto",
+        "species_id": None,
+        "time": (-1,),
+    }
+
+
+@pytest.mark.skip("WIP")
+class Test_EnsMin_AffectedArea(_CreateReference):
+    # class Test_EnsMin_AffectedArea(_TestBase):
+    reference = "ref_cosmo2e_ens_min_affected_area"
+    setup_dct = {
+        "infile": INFILE_NAME,
+        "outfile": "plot.png",
+        "plot_type": "ens_minn",
+        "variable": "affected_area",
+        "deposition_type": "tot",
+        "integrate": True,
+        "combine_species": True,
+        "simulation_type": "ensemble",
+        "ens_member_id": ENS_MEMBER_IDS,
+        "lang": "de",
+        "domain": "auto",
+        "species_id": None,
+        "time": (-1,),
+    }
+
+
+# class Test_CloudArrivalTime(_CreateReference):
+class Test_CloudArrivalTime(_TestBase):
+    reference = "ref_cosmo2e_ens_cloud_arrival_time"
+    setup_dct = {
+        "infile": INFILE_NAME,
+        "outfile": "plot.png",
+        "plot_type": "ens_cloud_arrival_time",
+        "variable": "concentration",
+        "integrate": True,
+        "combine_species": True,
+        "simulation_type": "ensemble",
+        "ens_member_id": ENS_MEMBER_IDS,
+        "ens_param_mem_min": 3,
+        "ens_param_thr": 1e-9,
+        "lang": "de",
+        "domain": "ch",
+        "species_id": None,
+        "time": (0,),
         "level": (0,),
     }
