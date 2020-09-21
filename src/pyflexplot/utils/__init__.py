77942f30c659e9d7462b3bbdbe740b3370310f7f
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-09-21 10:20:10 +0200

    Bump version: 0.13.1 → 0.13.2

--- src/pyflexplot/utils/__init__.py
+++ src/pyflexplot/utils/__init__.py
@@ -6,6 +6,6 @@ from typing import List
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.13.1"
+__version__ = "0.13.2"
 
 __all__: List[str] = []
