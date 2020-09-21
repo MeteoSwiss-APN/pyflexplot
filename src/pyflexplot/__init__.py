2de2026f76d6e3bbf89e6bed8dc49bf2dbefacb1
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-09-21 17:59:39 +0200

    Bump version: 0.13.4 → 0.13.5

--- src/pyflexplot/__init__.py
+++ src/pyflexplot/__init__.py
@@ -4,7 +4,7 @@ Top-level package for PyFlexPlot.
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.13.4"
+__version__ = "0.13.5"
 
 # Standard library
 import logging as pylogging
