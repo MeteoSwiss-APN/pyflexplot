968202207e4f23019cc23a8ce66ef2a0d006790e
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-12-09 00:56:48 +0100

    add another test for cloud across dateline

--- tests/fast/pyflexplot/test_plotting/test_domain.py
+++ tests/fast/pyflexplot/test_plotting/test_domain.py
@@ -6,12 +6,23 @@ import numpy as np
 from pyflexplot.plotting.domain import CloudDomain
 
 
-def test_dateline():
+class TestCloudAcrossDateline:
+    """Test cloud domain for a cloud positioned across the dateline."""
+
     lat = np.arange(-85.0, 85.1, 10.0)
     lon = np.arange(-185.0, 185.1, 10.0)
     mask = np.zeros([lat.size, lon.size], np.bool)
     mask[11:15, :4] = True
     mask[11:15, -4:] = True
-    domain = CloudDomain(lat, lon, mask_nz=mask, periodic_lon=True)
-    bbox = domain.get_bbox_corners()
-    assert bbox == (155.0, -155.0, 25.0, 55.0)
+
+    def test_dateline_wrong(self):
+        """Results are wrong if domain is not specified as periodic."""
+        domain = CloudDomain(self.lat, self.lon, mask_nz=self.mask, periodic_lon=False)
+        bbox = domain.get_bbox_corners()
+        assert bbox == (-185.0, 185.0, 25.0, 55.0)
+
+    def test_dateline_ok(self):
+        """Results are correct if domain is specified as periodic."""
+        domain = CloudDomain(self.lat, self.lon, mask_nz=self.mask, periodic_lon=True)
+        bbox = domain.get_bbox_corners()
+        assert bbox == (155.0, -155.0, 25.0, 55.0)
