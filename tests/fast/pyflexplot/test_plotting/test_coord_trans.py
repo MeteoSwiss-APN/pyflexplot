5cbc071a29cc0df21194543f910757159a54afb8
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-12-09 15:56:55 +0100

    add more tests for CoordinateTransformer

--- tests/fast/pyflexplot/test_plotting/test_coord_trans.py
+++ tests/fast/pyflexplot/test_plotting/test_coord_trans.py
@@ -17,16 +17,16 @@ from pyflexplot.plotting.coord_trans import CoordinateTransformer
 class Cfg:
     xy_in: Tuple[float, float]
     xy_out: Tuple[float, float]
-    clon_map: float = 0.0
-    clon_data: float = 0.0
-    clon_geo: float = 0.0
+    c_map: float = 0.0
+    c_data: float = 0.0
+    c_geo: float = 0.0
 
     @property
     def clons(self):
         return {
-            "clon_map": self.clon_map,
-            "clon_data": self.clon_data,
-            "clon_geo": self.clon_geo,
+            "clon_map": self.c_map,
+            "clon_data": self.c_data,
+            "clon_geo": self.c_geo,
         }
 
 
@@ -48,9 +48,25 @@ class TestRegLatLon:
     @pytest.mark.parametrize(
         "cfg",
         [
-            Cfg((20, 50), (0.4, 0.75)),  # [cfg0]
+            Cfg((0.2, -0.5), (0, 0)),  # [cfg0]
+            Cfg((0.5, 0.5), (30, 40)),  # [cfg1]
+            Cfg((1.1, -0.25), (90, 10)),  # [cfg2]
+            Cfg((0.2, -0.5), (180, 0), c_map=180),  # [cfg3]
+            Cfg((-1.6, -0.5), (0, 0), c_map=180),  # [cfg4]
+        ],
+    )
+    def test_axes_to_geo(self, cfg):
+        trans = self.trans(**cfg.clons)
+        assert np.allclose(trans.axes_to_geo(*cfg.xy_in), cfg.xy_out)
+
+    @pytest.mark.parametrize(
+        "cfg",
+        [
+            Cfg((0, 0), (0.2, -0.5)),  # [cfg0]
             Cfg((30, 40), (0.5, 0.5)),  # [cfg1]
             Cfg((90, 10), (1.1, -0.25)),  # [cfg2]
+            Cfg((180, 0), (0.2, -0.5), c_map=180),  # [cfg3]
+            Cfg((0, 0), (-1.6, -0.5), c_map=180),  # [cfg4]
         ],
     )
     def test_geo_to_axes(self, cfg):
@@ -60,11 +76,25 @@ class TestRegLatLon:
     @pytest.mark.parametrize(
         "cfg",
         [
-            Cfg((0.4, 0.75), (20, 50)),  # [cfg0]
-            Cfg((0.5, 0.5), (30, 40)),  # [cfg1]
-            Cfg((1.1, -0.25), (90, 10)),  # [cfg2]
+            Cfg((20, 50), (20, 50)),  # [cfg0]
+            Cfg((30, 40), (30, 40)),  # [cfg1]
+            Cfg((90, 10), (90, 10)),  # [cfg2]
+            Cfg((20, 50), (20, 50), c_map=180),  # [cfg3]
         ],
     )
-    def test_axes_to_geo(self, cfg):
+    def test_data_to_geo(self, cfg):
         trans = self.trans(**cfg.clons)
-        assert np.allclose(trans.axes_to_geo(*cfg.xy_in), cfg.xy_out)
+        assert np.allclose(trans.data_to_geo(*cfg.xy_in), cfg.xy_out)
+
+    @pytest.mark.parametrize(
+        "cfg",
+        [
+            Cfg((20, 50), (20, 50)),  # [cfg0]
+            Cfg((30, 40), (30, 40)),  # [cfg1]
+            Cfg((90, 10), (90, 10)),  # [cfg2]
+            Cfg((20, 50), (20, 50), c_map=180),  # [cfg3]
+        ],
+    )
+    def test_geo_to_data(self, cfg):
+        trans = self.trans(**cfg.clons)
+        assert np.allclose(trans.geo_to_data(*cfg.xy_in), cfg.xy_out)
