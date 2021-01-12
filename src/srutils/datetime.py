edab095aa9eed498ea33374985c85264987b5749
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-01-12 18:10:25 +0100

    add function srutils.datetime.datetime_range with tests

--- src/srutils/datetime.py
+++ src/srutils/datetime.py
@@ -2,9 +2,17 @@
 # Standard library
 import time
 from datetime import datetime
+from datetime import timedelta
 from datetime import timezone
 from itertools import starmap
+from typing import Callable
+from typing import List
+from typing import overload
+from typing import TypeVar
 from typing import Union
+
+# Third-party
+from typing_extensions import Literal
 
 
 def init_datetime(raw: Union[int, str]) -> datetime:
@@ -38,3 +46,64 @@ def derive_datetime_fmt(raw: Union[int, str]) -> str:
         ) from e
     else:
         return fmt
+
+
+ConvertT = TypeVar("ConvertT", bound=Union[int, str])
+
+
+@overload
+def datetime_range(
+    start: Union[datetime, int, str],
+    end: Union[datetime, int, str],
+    step: Union[timedelta, int, str],
+    *,
+    convert: Literal[None] = None,
+    fmt: str = ...,
+) -> List[datetime]:
+    ...
+
+
+@overload
+def datetime_range(
+    start: Union[datetime, int, str],
+    end: Union[datetime, int, str],
+    step: Union[timedelta, int, str],
+    *,
+    convert: Callable[[str], ConvertT],
+    fmt: str = ...,
+) -> List[ConvertT]:
+    ...
+
+
+def datetime_range(start, end, step, *, convert=None, fmt="%Y%m%d%H%M%S"):
+    """Create a range of ``datetime`` objects.
+
+    Args:
+        start: First time step.
+
+        end: Last time step.
+
+        step: Time step duration in seconds.
+
+        convert (optional): Return objects of this type instead of ``datetime``;
+            formatted with ``fmt`` before conversion.
+
+        fmt (optional): Format string applied to returned datetimes; only
+            relevant if ``convert`` is not None.
+
+    """
+    if not isinstance(start, datetime):
+        start = init_datetime(str(start))
+    if not isinstance(end, datetime):
+        end = init_datetime(str(end))
+    if start > end:
+        raise ValueError(f"start after end: {start} > {end}")
+    if not isinstance(step, timedelta):
+        step = timedelta(seconds=int(step))
+    steps: List[datetime] = [start]
+    while steps[-1] < end:
+        steps.append(steps[-1] + step)
+    if convert:
+        strs = [step.strftime(fmt) for step in steps]
+        return list(map(convert, strs))
+    return steps
