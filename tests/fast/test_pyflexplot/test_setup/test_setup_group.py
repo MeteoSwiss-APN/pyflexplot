27d1991be0400a3cbb51aeccd1abe950cbaec1ac
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-01-26 14:20:08 +0100

    add first test for SetupGroup.from_raw_params

--- tests/fast/test_pyflexplot/test_setup/test_setup_group.py
+++ tests/fast/test_pyflexplot/test_setup/test_setup_group.py
@@ -1,11 +1,11 @@
 """Tests for class ``pyflexplot.setup.SetupGroup``."""
-# Standard library
-
 # Third-party
+import pytest
 
 # First-party
 from pyflexplot.setup import Setup
 from pyflexplot.setup import SetupGroup
+from srutils.testing import check_is_sub_element
 
 
 class TestCopy:
@@ -20,3 +20,34 @@ class TestCopy:
         copied_setups = setups.copy()
         assert len(copied_setups) == len(setups)
         assert copied_setups == setups
+
+
+class Test_FromRawParams:
+    def test_empty(self):
+        with pytest.raises(ValueError):
+            SetupGroup.from_raw_params([])
+
+    def test_one_variable(self):
+        raw_params = {
+            "infile": "foo.nc",
+            "outfile": "foo.png",
+            "input_variable": "concentration",
+            "species_id": [1, 2],
+            "combine_species": False,
+        }
+        setups = SetupGroup.from_raw_params(raw_params)
+        res = setups.dicts()
+        sol = [
+            {
+                "infile": "foo.nc",
+                "outfile": "foo.png",
+                "core": {
+                    "input_variable": "concentration",
+                    "combine_species": False,
+                    "dimensions": {
+                        "species_id": (1, 2),
+                    },
+                },
+            },
+        ]
+        check_is_sub_element(sol, res, "solution", "result")
