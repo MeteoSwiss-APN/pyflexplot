71a07325437e6fc3bfc1ad19de6682e1f0559e94
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-02-24 13:04:56 +0100

    group base decompression tests

--- tests/fast/test_pyflexplot/test_setups/test_plot_setup.py
+++ tests/fast/test_pyflexplot/test_setups/test_plot_setup.py
@@ -62,110 +62,111 @@ class Test_Create:
 
 
 class Test_Decompress:
-    @property
-    def setup(self):
-        return DEFAULT_SETUP.derive(
-            {
-                "panels": [
-                    {
-                        "plot_variable": "tot_deposition",
-                        "combine_species": True,
-                        "dimensions": {
-                            "nageclass": (0,),
-                            "noutrel": (0,),
-                            "numpoint": (0,),
-                            "species_id": [1, 2],
-                            "time": [1, 2, 3],
-                        },
-                    }
-                ],
-            },
-        )
-
-    def test_full_fail(self):
-        """Full decompression fails with internal=True due to ens_member_id."""
-        with pytest.raises(ValueError, match="ens_member_id"):
-            self.setup.decompress()
-
-    def test_full_external(self):
-        """Decompress all params."""
-        setups = self.setup.decompress(internal=False)
-        assert len(setups) == 6
-        res = {
-            (
-                s.panels.collect_equal("dimensions").variable,
-                s.panels.collect_equal("dimensions").species_id,
-                s.panels.collect_equal("dimensions").time,
+    class Test_Base:
+        @property
+        def setup(self):
+            return DEFAULT_SETUP.derive(
+                {
+                    "panels": [
+                        {
+                            "plot_variable": "tot_deposition",
+                            "combine_species": True,
+                            "dimensions": {
+                                "nageclass": (0,),
+                                "noutrel": (0,),
+                                "numpoint": (0,),
+                                "species_id": [1, 2],
+                                "time": [1, 2, 3],
+                            },
+                        }
+                    ],
+                },
             )
-            for s in setups
-        }
-        sol = {
-            (("dry_deposition", "wet_deposition"), 1, 1),
-            (("dry_deposition", "wet_deposition"), 1, 2),
-            (("dry_deposition", "wet_deposition"), 1, 3),
-            (("dry_deposition", "wet_deposition"), 2, 1),
-            (("dry_deposition", "wet_deposition"), 2, 2),
-            (("dry_deposition", "wet_deposition"), 2, 3),
-        }
-        assert res == sol
 
-    def test_partially_one(self):
-        """Decompress only one select parameter."""
-        setups = self.setup.decompress(["dimensions.species_id"])
-        assert isinstance(setups, PlotSetupGroup)
-        assert all(isinstance(setup, PlotSetup) for setup in setups)
-        assert len(setups) == 2
-        res = {
-            (
-                s.panels.collect_equal("dimensions").variable,
-                s.panels.collect_equal("dimensions").species_id,
-                s.panels.collect_equal("dimensions").time,
-            )
-            for s in setups
-        }
-        sol = {
-            (("dry_deposition", "wet_deposition"), 1, (1, 2, 3)),
-            (("dry_deposition", "wet_deposition"), 2, (1, 2, 3)),
-        }
-        assert res == sol
+        def test_full_fail(self):
+            """Full decompression fails with internal=True due to ens_member_id."""
+            with pytest.raises(ValueError, match="ens_member_id"):
+                self.setup.decompress()
 
-    def test_select_two(self):
-        """Decompress two select parameters."""
-        setups = self.setup.decompress(["dimensions.time", "dimensions.species_id"])
-        assert len(setups) == 6
-        assert isinstance(setups, PlotSetupGroup)
-        assert all(isinstance(setup, PlotSetup) for setup in setups)
-        res = {
-            (
-                s.panels.collect_equal("dimensions").variable,
-                s.panels.collect_equal("dimensions").species_id,
-                s.panels.collect_equal("dimensions").time,
-            )
-            for s in setups
-        }
-        sol = {
-            (("dry_deposition", "wet_deposition"), 1, 1),
-            (("dry_deposition", "wet_deposition"), 1, 2),
-            (("dry_deposition", "wet_deposition"), 1, 3),
-            (("dry_deposition", "wet_deposition"), 2, 1),
-            (("dry_deposition", "wet_deposition"), 2, 2),
-            (("dry_deposition", "wet_deposition"), 2, 3),
-        }
-        assert res == sol
+        def test_full_external(self):
+            """Decompress all params."""
+            setups = self.setup.decompress(internal=False)
+            assert len(setups) == 6
+            res = {
+                (
+                    s.panels.collect_equal("dimensions").variable,
+                    s.panels.collect_equal("dimensions").species_id,
+                    s.panels.collect_equal("dimensions").time,
+                )
+                for s in setups
+            }
+            sol = {
+                (("dry_deposition", "wet_deposition"), 1, 1),
+                (("dry_deposition", "wet_deposition"), 1, 2),
+                (("dry_deposition", "wet_deposition"), 1, 3),
+                (("dry_deposition", "wet_deposition"), 2, 1),
+                (("dry_deposition", "wet_deposition"), 2, 2),
+                (("dry_deposition", "wet_deposition"), 2, 3),
+            }
+            assert res == sol
 
-    def test_select_variable_fails(self):
-        """``Dimensions.variable`` cannot be selected for decompression."""
-        msg = r"^cannot decompress Dimensions.variable"
-        with pytest.raises(ValueError, match=msg):
-            self.setup.decompress(["dimensions.variable"])
+        def test_partially_one(self):
+            """Decompress only one select parameter."""
+            setups = self.setup.decompress(["dimensions.species_id"])
+            assert isinstance(setups, PlotSetupGroup)
+            assert all(isinstance(setup, PlotSetup) for setup in setups)
+            assert len(setups) == 2
+            res = {
+                (
+                    s.panels.collect_equal("dimensions").variable,
+                    s.panels.collect_equal("dimensions").species_id,
+                    s.panels.collect_equal("dimensions").time,
+                )
+                for s in setups
+            }
+            sol = {
+                (("dry_deposition", "wet_deposition"), 1, (1, 2, 3)),
+                (("dry_deposition", "wet_deposition"), 2, (1, 2, 3)),
+            }
+            assert res == sol
 
-    def test_model(self):
-        """Decompress model parameters."""
-        setup = self.setup.derive({"model": {"ens_member_id": (0, 1, 2)}})
-        setups = setup.decompress(["model.ens_member_id"], internal=False)
-        assert len(setups) == 3
-        assert isinstance(setups, list)
-        assert all(isinstance(setup, PlotSetup) for setup in setups)
-        res = {setup.model.ens_member_id for setup in setups}
-        sol = {(0,), (1,), (2,)}
-        assert res == sol
+        def test_select_two(self):
+            """Decompress two select parameters."""
+            setups = self.setup.decompress(["dimensions.time", "dimensions.species_id"])
+            assert len(setups) == 6
+            assert isinstance(setups, PlotSetupGroup)
+            assert all(isinstance(setup, PlotSetup) for setup in setups)
+            res = {
+                (
+                    s.panels.collect_equal("dimensions").variable,
+                    s.panels.collect_equal("dimensions").species_id,
+                    s.panels.collect_equal("dimensions").time,
+                )
+                for s in setups
+            }
+            sol = {
+                (("dry_deposition", "wet_deposition"), 1, 1),
+                (("dry_deposition", "wet_deposition"), 1, 2),
+                (("dry_deposition", "wet_deposition"), 1, 3),
+                (("dry_deposition", "wet_deposition"), 2, 1),
+                (("dry_deposition", "wet_deposition"), 2, 2),
+                (("dry_deposition", "wet_deposition"), 2, 3),
+            }
+            assert res == sol
+
+        def test_select_variable_fails(self):
+            """``Dimensions.variable`` cannot be selected for decompression."""
+            msg = r"^cannot decompress Dimensions.variable"
+            with pytest.raises(ValueError, match=msg):
+                self.setup.decompress(["dimensions.variable"])
+
+        def test_model(self):
+            """Decompress model parameters."""
+            setup = self.setup.derive({"model": {"ens_member_id": (0, 1, 2)}})
+            setups = setup.decompress(["model.ens_member_id"], internal=False)
+            assert len(setups) == 3
+            assert isinstance(setups, list)
+            assert all(isinstance(setup, PlotSetup) for setup in setups)
+            res = {setup.model.ens_member_id for setup in setups}
+            sol = {(0,), (1,), (2,)}
+            assert res == sol
