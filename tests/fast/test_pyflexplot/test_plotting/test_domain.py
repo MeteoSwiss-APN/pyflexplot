69c155bff55a887fe00d7ec435cfd355b6696236
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-03-08 22:03:08 +0100

    fix bug in CloudDomain.find_bbox_corners and adapt/add tests

--- tests/fast/test_pyflexplot/test_plotting/test_domain.py
+++ tests/fast/test_pyflexplot/test_plotting/test_domain.py
@@ -6,14 +6,14 @@ import numpy as np
 from pyflexplot.plotting.domain import CloudDomain
 
 
-class TestCloudAcrossDateline:
+class Test_CloudAcrossDateline:
     """Test cloud domain for a cloud positioned across the dateline."""
 
     lat = np.arange(-85.0, 85.1, 10.0)
     lon = np.arange(-185.0, 185.1, 10.0)
     mask = np.zeros([lat.size, lon.size], np.bool)
-    mask[11:15, :4] = True
-    mask[11:15, -4:] = True
+    mask[10:17, :2] = True
+    mask[10:17, -3:] = True
 
     def test_dateline_wrong(self):
         """Results are wrong if domain is not specified as periodic."""
@@ -21,7 +21,7 @@ class TestCloudAcrossDateline:
             self.lat, self.lon, mask=self.mask, config={"periodic_lon": False}
         )
         bbox = domain.find_bbox_corners()
-        assert bbox == (-185.0, 185.0, 25.0, 55.0)
+        assert bbox == (-185.0, 185.0, 15.0, 75.0)
 
     def test_dateline_ok(self):
         """Results are correct if domain is specified as periodic."""
@@ -29,4 +29,16 @@ class TestCloudAcrossDateline:
             self.lat, self.lon, mask=self.mask, config={"periodic_lon": True}
         )
         bbox = domain.find_bbox_corners()
-        assert bbox == (155.0, -155.0, 25.0, 55.0)
+        assert bbox == (165.0, -175.0, 15.0, 75.0)
+
+    def test_aspect(self):
+        """Adapt the domain to a given aspect ratio."""
+        """Results are correct if domain is specified as periodic."""
+        domain = CloudDomain(
+            self.lat,
+            self.lon,
+            mask=self.mask,
+            config={"periodic_lon": True, "aspect": 1.5},
+        )
+        bbox = domain.find_bbox_corners()
+        assert bbox == (130.0, -140.0, 15.0, 75.0)
