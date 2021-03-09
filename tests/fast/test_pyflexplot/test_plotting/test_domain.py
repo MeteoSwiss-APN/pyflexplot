eace497cfd1e8b0a20bbdcf6d7c83cfac957f8be
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-03-09 12:03:13 +0100

    reorganize CloudDomain tests

--- tests/fast/test_pyflexplot/test_plotting/test_domain.py
+++ tests/fast/test_pyflexplot/test_plotting/test_domain.py
@@ -6,39 +6,69 @@ import numpy as np
 from pyflexplot.plotting.domain import CloudDomain
 
 
-class Test_CloudAcrossDateline:
-    """Test cloud domain for a cloud positioned across the dateline."""
+class Test_CloudDomain:
+    """Test cloud domain."""
 
-    lat = np.arange(-85.0, 85.1, 10.0)
-    lon = np.arange(-185.0, 185.1, 10.0)
-    mask = np.zeros([lat.size, lon.size], np.bool)
-    mask[10:17, :2] = True
-    mask[10:17, -3:] = True
+    class Global:
+        """Global domain."""
 
-    def test_dateline_wrong(self):
-        """Results are wrong if domain is not specified as periodic."""
-        domain = CloudDomain(
-            self.lat, self.lon, mask=self.mask, config={"periodic_lon": False}
-        )
-        bbox = domain.find_bbox_corners()
-        assert bbox == (-185.0, 185.0, 15.0, 75.0)
+        lat = np.arange(-85, 85.1, 10)
+        lon = np.arange(-175, 175.1, 10)
 
-    def test_dateline_ok(self):
-        """Results are correct if domain is specified as periodic."""
-        domain = CloudDomain(
-            self.lat, self.lon, mask=self.mask, config={"periodic_lon": True}
-        )
-        bbox = domain.find_bbox_corners()
-        assert bbox == (165.0, -175.0, 15.0, 75.0)
+        def get_mask(self, lllon, urlon, lllat, urlat):
+            mask = np.zeros([self.lat.size, self.lon.size], np.bool)
+            if lllon < 0:
+                mask[lllat:urlat, lllon:] = True
+                mask[lllat:urlat, :urlon] = True
+            else:
+                mask[lllat:urlat, lllon:urlon] = True
+            return mask
 
-    def test_aspect(self):
-        """Adapt the domain to a given aspect ratio."""
-        """Results are correct if domain is specified as periodic."""
-        domain = CloudDomain(
-            self.lat,
-            self.lon,
-            mask=self.mask,
-            config={"periodic_lon": True, "aspect": 1.5},
-        )
-        bbox = domain.find_bbox_corners()
-        assert bbox == (130.0, -140.0, 15.0, 75.0)
+        def get_domain(self, lllon, urlon, lllat, urlat, **config):
+            mask = self.get_mask(lllon, urlon, lllat, urlat)
+            return CloudDomain(self.lat, self.lon, mask=mask, config=config)
+
+        def get_bbox(self, lllon, urlon, lllat, urlat, **config):
+            domain = self.get_domain(lllon, urlon, lllat, urlat, **config)
+            return domain.find_bbox_corners()
+
+    class Test_AcrossDateline(Global):
+        """Cloud stretching across dateline."""
+
+        def test_non_periodic_wrong(self):
+            """Results are wrong if domain is not specified as periodic."""
+            bbox = self.get_bbox(-3, 2, 10, 17, periodic_lon=False)
+            assert bbox == (-175, 175, 15, 75)
+
+        def test_periodic_ok(self):
+            """Results are correct if domain is specified as periodic."""
+            bbox = self.get_bbox(-3, 2, 10, 17, periodic_lon=True)
+            assert bbox == (155, -165, 15, 75)
+
+        def test_zonal_no_aspect(self):
+            """Zonally extended cloud, w/o specified aspect ratio."""
+            bbox = self.get_bbox(-5, 4, 13, 15, periodic_lon=True)
+            # dlon, dlat = 80, 10; aspect = 8
+            assert bbox == (135, -145, 45, 55)
+
+        def test_zonal_aspect(self):
+            """Zonally extended cloud, with specified aspect ratio."""
+            bbox = self.get_bbox(-5, 4, 13, 15, periodic_lon=True, aspect=1.5)
+            # dlat = dlon / aspect = 80 / 1.5 = 53.333
+            # ddlat = 53.333 - 10 = 43.333
+            ddlat2 = (43 + 1 / 3) / 2
+            assert bbox == (135, -145, 45 - ddlat2, 55 + ddlat2)
+
+        def test_meridional_no_aspect(self):
+            """Meridionally extended cloud, w/o specified aspect ratio."""
+            bbox = self.get_bbox(-1, 2, 10, 17, periodic_lon=True)
+            # dlon, dlat = 20, 60; aspect = 0.333
+            assert bbox == (175, -165, 15, 75)
+
+        def test_meridional_aspect(self):
+            """Meridionally extended cloud, with specified aspect ratio."""
+            bbox = self.get_bbox(-1, 2, 10, 17, periodic_lon=True, aspect=1.5)
+            # dlon = aspect * dlat = 1.5 * 60 = 90
+            # ddlon = 90 - 20 = 70
+            ddlon2 = 70 / 2
+            assert bbox == (175 - ddlon2, -165 + ddlon2, 15, 75)
