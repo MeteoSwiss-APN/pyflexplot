45f409d6e1f720f5e75fb6809ebe1d7352b44ca2
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-03-09 15:38:22 +0100

    new version v0.14.4 (patch bump)
    fix issues with cloud domain

--- src/pyflexplot/utils/__init__.py
+++ src/pyflexplot/utils/__init__.py
@@ -4,6 +4,6 @@ from typing import List
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.14.3"
+__version__ = "0.14.4"
 
 __all__: List[str] = []
