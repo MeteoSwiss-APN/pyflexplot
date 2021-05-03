677019c59490adec99553ac33427ea58ba3bcbe8
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-05-03 09:04:25 +0200

    new version v0.15.4 (patch bump)
    support multi-release files; fix ensemble affected area; etc.

--- src/pyflexplot/__init__.py
+++ src/pyflexplot/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.15.3"
+__version__ = "0.15.4"
 
 # Standard library
 import logging as _logging
