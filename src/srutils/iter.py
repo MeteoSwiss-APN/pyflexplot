e0efbbfcaa2d14ebf81a49476d8172eba220af75
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2021-09-09 10:56:09 +0200

    force out-of-range time indices into range

--- src/srutils/iter.py
+++ src/srutils/iter.py
@@ -49,14 +49,21 @@ def isiterable(obj, str_ok=True):
         return True
 
 
-def resolve_negative_indices(idcs: Sequence[int], n: int) -> Sequence[int]:
-    """Turn negative indices into positive ones, given the sequence length.
+def resolve_indices(
+    idcs: Sequence[int], n: int, force_in_range: bool = False
+) -> Sequence[int]:
+    """Resolve indices, like subtracting negative indices from the end.
 
     Args:
         idcs: Indices. Not required to be consecutive, in order, or unique.
             May be positive (from start) or negative (from end).
 
         n: Length of the sequence which the indices refer to.
+
+        force_in_range (optional): Instead of raising an exception when an index
+            is out of range (i.e., negative or equal or larger than ``n`` after
+            negative index resolution), force it into the range (set it to zero
+            or ``n - 1``).
 
     """
     idcs_new = []
@@ -65,8 +72,12 @@ def resolve_negative_indices(idcs: Sequence[int], n: int) -> Sequence[int]:
         if i < 0:
             i += n
         if i < 0:
-            raise ValueError(f"invalid index {i_in}: {i} < 0")
+            if not force_in_range:
+                raise ValueError(f"invalid index {i_in}: {i} < 0")
+            i = 0
         if i >= n:
-            raise ValueError(f"invalid index {i_in}: {i} >= {n}")
+            if not force_in_range:
+                raise ValueError(f"invalid index {i_in}: {i} >= {n}")
+            i = n - 1
         idcs_new.append(i)
     return type(idcs)(idcs_new)  # type: ignore
