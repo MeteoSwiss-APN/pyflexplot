bae69c800183ac0b141b3f329254b4fe47c52084
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2021-09-28 14:12:29 +0200

    new version v1.0.0 (major bump)
    ""

--- setup.pip-venv.py
+++ setup.pip-venv.py
@@ -31,7 +31,7 @@ description_files = [
 
 metadata = {
     "name": "pyflexplot",
-    "version": "0.15.5",
+    "version": "1.0.0",
     "description": "PyFlexPlot visualizes FLEXPART particle dispersion simulations.",
     "long_description": read_present_files(description_files),
     "author": "Stefan Ruedisuehli",
--- src/pyflexplot/__init__.py
+++ src/pyflexplot/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Stefan Ruedisuehli"""
 __email__ = "stefan.ruedisuehli@env.ethz.ch"
-__version__ = "0.15.5"
+__version__ = "1.0.0"
 
 # Standard library
 import logging as _logging
