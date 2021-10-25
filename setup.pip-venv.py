58d3378a2a26872d8f017f96c32fdd4029d10290
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2021-10-25 14:02:22 +0200

    new version v1.0.1 (patch bump)
    Bugfix for Flexpart output with only one timestep.

--- setup.pip-venv.py
+++ setup.pip-venv.py
@@ -31,7 +31,7 @@ description_files = [
 
 metadata = {
     "name": "pyflexplot",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "description": "PyFlexPlot visualizes FLEXPART particle dispersion simulations.",
     "long_description": read_present_files(description_files),
     "author": "Stefan Ruedisuehli",
