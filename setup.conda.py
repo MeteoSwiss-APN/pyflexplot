c4ca8a445aa3c505a6dedcebd5d0b53191ebe7c2
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2021-12-03 09:03:51 +0100

    new version v1.0.5 (patch bump)
    Increased speed of city projection.

--- setup.conda.py
+++ setup.conda.py
@@ -32,7 +32,7 @@ description_files = [
 
 metadata = {
     "name": "pyflexplot",
-    "version": "1.0.4",
+    "version": "1.0.5",
     "description": "PyFlexPlot visualizes FLEXPART particle dispersion simulations.",
     "long_description": read_present_files(description_files),
     "author": "Stefan Ruedisuehli",
