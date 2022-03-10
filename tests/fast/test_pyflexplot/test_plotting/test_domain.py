66c1002cbfbbf0f50d940f1cf179edf73b64e3c6
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2022-03-10 13:57:48 +0100

    rename some variables and methods

--- tests/fast/test_pyflexplot/test_plotting/test_domain.py
+++ tests/fast/test_pyflexplot/test_plotting/test_domain.py
@@ -24,53 +24,54 @@ class Test_GlobalCloudDomain:
 
         def get_mask(self, bboxes):
             mask = np.zeros([self.lat.size, self.lon.size], np.bool_)
-            for lllon, urlon, lllat, urlat in bboxes:
-                if lllon < 0:
-                    mask[lllat:urlat, lllon:] = True
-                    mask[lllat:urlat, :urlon] = True
+            for idx_lllon, idx_urlon, idx_lllat, idx_urlat in bboxes:
+                if idx_lllon < 0:
+                    mask[idx_lllat:idx_urlat, idx_lllon:] = True
+                    mask[idx_lllat:idx_urlat, :idx_urlon] = True
                 else:
-                    mask[lllat:urlat, lllon:urlon] = True
+                    mask[idx_lllat:idx_urlat, idx_lllon:idx_urlon] = True
+            assert mask.any(), f"invalid bboxes (empty mask): {bboxes}"
             return mask
 
         def get_domain(self, *mask_args, **config):
             mask = self.get_mask(*mask_args)
             return CloudDomain(self.lat, self.lon, mask=mask, config=config)
 
-        def get_bbox(self, *mask_args, **config):
+        def get_bbox_corners(self, *mask_args, **config):
             domain = self.get_domain(*mask_args, **config)
             return domain.find_bbox_corners()
 
     class Continuous(General):
         """Continuous cloud."""
 
-        def get_mask(self, lllon, urlon, lllat, urlat):
-            return super().get_mask([(lllon, urlon, lllat, urlat)])
+        def get_mask(self, *bbox_idcs):
+            return super().get_mask([bbox_idcs])
 
     class Test_ZonalNearPole(Continuous):
         """Zonally extended cloud near a pole."""
 
         def test_north_no_aspect(self):
             """Near north pole, w/o specified aspect ratio."""
-            bbox = self.get_bbox(10, 29, 15, 17)
+            bbox = self.get_bbox_corners(10, 29, 15, 17)
             # dlon, dlat = 180, 10; aspect = 18
             assert bbox == (-75, 105, 65, 75)
 
         def test_north_aspect(self):
             """Near north pole, with specified aspect ratio."""
-            bbox = self.get_bbox(10, 29, 16, 18, aspect=2)
+            bbox = self.get_bbox_corners(10, 29, 16, 18, aspect=2)
             # dlat = dlon / aspect = 180 / 2 = 90
             # ddlat = 90 - 10 = 80
             assert bbox == (-75, 105, -5, 85)
 
         def test_south_no_aspect(self):
             """Near south pole, w/o specified aspect ratio."""
-            bbox = self.get_bbox(15, 24, 1, 4)
+            bbox = self.get_bbox_corners(15, 24, 1, 4)
             # dlon, dlat = 80, 20; aspect = 4
             assert bbox == (-25, 55, -75, -55)
 
         def test_south_aspect(self):
             """Near south pole, with specified aspect ratio."""
-            bbox = self.get_bbox(15, 24, 1, 4, aspect=1)
+            bbox = self.get_bbox_corners(15, 24, 1, 4, aspect=1)
             # dlat = dlon / aspect = 80 / 1 = 80
             # ddlat = 80 - 20 = 60
             assert bbox == (-25, 55, -85, -5)
@@ -80,23 +81,23 @@ class Test_GlobalCloudDomain:
 
         def test_non_periodic_wrong(self):
             """Results are wrong if domain is not specified as periodic."""
-            bbox = self.get_bbox(-3, 2, 10, 17, periodic_lon=False)
+            bbox = self.get_bbox_corners(-3, 2, 10, 17, periodic_lon=False)
             assert bbox == (-175, 175, 15, 75)
 
         def test_periodic_ok(self):
             """Results are correct if domain is specified as periodic."""
-            bbox = self.get_bbox(-3, 2, 10, 17, periodic_lon=True)
+            bbox = self.get_bbox_corners(-3, 2, 10, 17, periodic_lon=True)
             assert bbox == (155, -165, 15, 75)
 
         def test_zonal_no_aspect(self):
             """Zonally extended cloud, w/o specified aspect ratio."""
-            bbox = self.get_bbox(-5, 4, 13, 15, periodic_lon=True)
+            bbox = self.get_bbox_corners(-5, 4, 13, 15, periodic_lon=True)
             # dlon, dlat = 80, 10; aspect = 8
             assert bbox == (135, -145, 45, 55)
 
         def test_zonal_aspect(self):
             """Zonally extended cloud, with specified aspect ratio."""
-            bbox = self.get_bbox(-5, 4, 13, 15, periodic_lon=True, aspect=1.5)
+            bbox = self.get_bbox_corners(-5, 4, 13, 15, periodic_lon=True, aspect=1.5)
             # dlat = dlon / aspect = 80 / 1.5 = 53.333
             # ddlat = 53.333 - 10 = 43.333
             ddlat2 = (43 + 1 / 3) / 2
@@ -104,13 +105,13 @@ class Test_GlobalCloudDomain:
 
         def test_meridional_no_aspect(self):
             """Meridionally extended cloud, w/o specified aspect ratio."""
-            bbox = self.get_bbox(-1, 2, 10, 17, periodic_lon=True)
+            bbox = self.get_bbox_corners(-1, 2, 10, 17, periodic_lon=True)
             # dlon, dlat = 20, 60; aspect = 0.333
             assert bbox == (175, -165, 15, 75)
 
         def test_meridional_aspect(self):
             """Meridionally extended cloud, with specified aspect ratio."""
-            bbox = self.get_bbox(-1, 2, 10, 17, periodic_lon=True, aspect=1.5)
+            bbox = self.get_bbox_corners(-1, 2, 10, 17, periodic_lon=True, aspect=1.5)
             # dlon = aspect * dlat = 1.5 * 60 = 90
             # ddlon = 90 - 20 = 70
             ddlon2 = 70 / 2
@@ -121,17 +122,19 @@ class Test_GlobalCloudDomain:
 
         def test_continuous(self):
             """W/o specified aspect ratio but with periodic zonal boundaries."""
-            bbox = self.get_bbox([(-5, 4, 10, 17)], periodic_lon=True)
+            bbox = self.get_bbox_corners([(-5, 4, 10, 17)], periodic_lon=True)
             assert bbox == (135, -145, 15, 75)
 
         def test_non_continuous_non_period(self):
             """W/o specified aspect ratio and w/o periodic zonal boundaries."""
-            bbox = self.get_bbox([(30, 33, 10, 17), (1, 4, 10, 17)])
+            bbox = self.get_bbox_corners([(30, 33, 10, 17), (1, 4, 10, 17)])
             assert bbox == (-165, 145, 15, 75)
 
         def test_non_continuous_periodic(self):
             """W/o specified aspect ratio but with periodic zonal boundaries."""
-            bbox = self.get_bbox([(31, 33, 10, 17), (1, 4, 10, 17)], periodic_lon=True)
+            bbox = self.get_bbox_corners(
+                [(31, 33, 10, 17), (1, 4, 10, 17)], periodic_lon=True
+            )
             assert bbox == (135, -145, 15, 75)
 
 
