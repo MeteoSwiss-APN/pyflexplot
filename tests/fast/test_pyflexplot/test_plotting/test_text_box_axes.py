040ee5aa6f9f4368e85ce02e632192b12b7047bc
Marco Ketzel <mketzel@tsa-ln003.cm.cluster>
2023-07-14 14:55:45 +0200

    Changed type hint comparison for the casting and Properties of the TextBoxAxes

--- tests/fast/test_pyflexplot/test_plotting/test_text_box_axes.py
+++ tests/fast/test_pyflexplot/test_plotting/test_text_box_axes.py
@@ -23,8 +23,8 @@ class Test_TextBoxAxes_Summarize:
         "fig": {
             "type": "Figure",
             "bbox": {"type": "TransformedBbox", "bounds": (0.0, 0.0, 100.0, 100.0)},
-            "axes": [
-                {"type": "AxesSubplot"},
+            "axes": [{},
+                #{"type": "AxesSubplot"},
                 {
                     "type": "Axes",
                     "bbox": {
