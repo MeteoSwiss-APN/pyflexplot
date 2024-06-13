201972f07e73bd93f740bf184c4bbada82c24669
Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
2024-06-13 09:13:01 +0200

    Convert units in Flexpart-ICON output by 1e-12 as for COSMO and IFS to get [Bq m-3] for concentrations and [Bq m-2] for deposition.

--- src/pyflexplot/input/fix_nc_input.py
+++ src/pyflexplot/input/fix_nc_input.py
@@ -14,7 +14,7 @@ from .meta_data import MetaData
 
 
 class FlexPartDataFixer:
-    """Fix issues with wrong units in Flexpart NetCDF output."""
+    """Fix issues with FlexPart NetCDF output."""
 
     cosmo_models = [
         "COSMO-2",
@@ -36,16 +36,21 @@ class FlexPartDataFixer:
     def fix_nc_var_fld(
         self, fld: np.ndarray, model: str, var_ncattrs: Mapping[str, Any]
     ) -> None:
+        """Convert wrong or inadequate units in Flexpart NetCDF output variables."""
         unit = var_ncattrs["units"]
         if model in self.cosmo_models:
-            if unit in ["ng kg-1", "1e-12 kg m-2"]:
+            if unit in ["ng kg-1", "1e-12 kg m-2", "1e-12 Bq m-3", "1e-12 Bq m-2"]:
                 fact = 1.0e-12
             else:
+                msg = f"model {model}: unrecognized unit '{unit}'; skip variable fixes"
+                log(wrn=msg)
                 return
         elif model in self.ifs_models:
-            if unit in ["ng m-3", "1e-12 kg m-2"]:
+            if unit in ["ng m-3", "1e-12 kg m-2", "1e-12 Bq m-3", "1e-12 Bq m-2"]:
                 fact = 1.0e-12
             else:
+                msg = f"model {model}: unrecognized unit '{unit}'; skip variable fixes"
+                log(wrn=msg)
                 return
         else:
             raise NotImplementedError(f"model '{model}'")
@@ -60,17 +65,16 @@ class FlexPartDataFixer:
         integrate: bool,
         mdata: Union[MetaData, Sequence[MetaData]],
     ) -> None:
+        """Adapt wrong or inadequate unit strings in Flexpart NetCDF output metadata."""
         if isinstance(mdata, Sequence):
             for mdata_i in mdata:
                 self.fix_meta_data(model, plot_variable, integrate, mdata_i)
             return
         unit = str(mdata.variable.unit)
         if model in self.cosmo_models:
-            if unit == "1e-12 Bq m-3":  # Correct, add time unit for integrated values
-                new_unit = "1e-12 Bq h m-3" if integrate else unit
-            elif unit == "ng kg-1":
+            if unit in ["ng kg-1", "1e-12 Bq m-3"]:
                 new_unit = "Bq h m-3" if integrate else "Bq m-3"
-            elif unit == "1e-12 kg m-2":
+            elif unit in ["1e-12 kg m-2", "1e-12 Bq m-2"]:
                 # new_unit = "Bq h m-2" if integrate else "Bq m-2"
                 new_unit = "Bq m-2"
             # SR_TMP <
@@ -84,13 +88,13 @@ class FlexPartDataFixer:
                 new_unit = "Bq m-2"
             # SR_TMP >
             else:
+                msg = f"model {model}: unrecognized unit '{unit}'; skip meta data fixes"
+                log(wrn=msg)
                 return
         elif model in self.ifs_models:
-            if unit == "1e-12 Bq m-3":  # Correct, add time unit for integrated values
-                new_unit = "1e-12 Bq h m-3" if integrate else unit
-            elif unit == "ng m-3":
+            if unit in ["ng m-3", "1e-12 Bq m-3"]:
                 new_unit = "Bq h m-3" if integrate else "Bq m-3"
-            elif unit == "1e-12 kg m-2":
+            elif unit in ["1e-12 kg m-2", "1e-12 Bq m-2"]:
                 # new_unit = "Bq h m-2" if integrate else "Bq m-2"
                 new_unit = "Bq m-2"
             # SR_TMP <
@@ -104,6 +108,8 @@ class FlexPartDataFixer:
                 new_unit = "Bq m-2"
             # SR_TMP >
             else:
+                msg = f"model {model}: unrecognized unit '{unit}'; skip meta data fixes"
+                log(wrn=msg)
                 return
         else:
             raise NotImplementedError(f"model '{model}'")
