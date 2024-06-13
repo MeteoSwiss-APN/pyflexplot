Correct units for Flexpart-ICON output. (#46)
* Update README.md to include instructions how to install Poetry at CSCS and
better explain testing with data on /store_new. Change presets to
use newly available test data for Flexpart-ICON. Remove unused test data
tests/data/pyflexplot/flexpart/original and tests/data/pytrajplot.

* Update main.py (#44)

* Unify styles of code blocks in README.

* Update model name in README example.

* Remove warning message when no unit correction in NetCDF file is necessary. Update REAMDE.md and increment patch level.

* Correct unit for integrated concentration (insert h as time unit). Do not print out warning messages for units that are not corrected. Modified AUTHORS to reflect that S.R. was the main author.

* Convert units in Flexpart-ICON output by 1e-12 as for COSMO and IFS to get [Bq m-3] for concentrations and [Bq m-2] for deposition.

* Update README.md

Co-authored-by: Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>

* Update README.md

Co-authored-by: Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>

---------

Co-authored-by: Victoria Cherkas <87643948+victoria-cherkas@users.noreply.github.com>

src/pyflexplot/input/fix_nc_input.py
@@ -36,16 +36,17 @@ class FlexPartDataFixer:
     def fix_nc_var_fld(
         self, fld: np.ndarray, model: str, var_ncattrs: Mapping[str, Any]
     ) -> None:
+        """Convert wrong or inadequate units in Flexpart NetCDF output variables."""
         unit = var_ncattrs["units"]
         if model in self.cosmo_models:
-            if unit in ["ng kg-1", "1e-12 kg m-2"]:
+            if unit in ["ng kg-1", "1e-12 kg m-2", "1e-12 Bq m-3", "1e-12 Bq m-2"]:
                 fact = 1.0e-12
             else:
                 msg = f"model {model}: unrecognized unit '{unit}'; skip variable fixes"
                 log(wrn=msg)
                 return
         elif model in self.ifs_models:
-            if unit in ["ng m-3", "1e-12 kg m-2"]:
+            if unit in ["ng m-3", "1e-12 kg m-2", "1e-12 Bq m-3", "1e-12 Bq m-2"]:
                 fact = 1.0e-12
             else:
                 msg = f"model {model}: unrecognized unit '{unit}'; skip variable fixes"
@@ -64,15 +65,16 @@ class FlexPartDataFixer:
         integrate: bool,
         mdata: Union[MetaData, Sequence[MetaData]],
     ) -> None:
+        """Adapt wrong or inadequate unit strings in Flexpart NetCDF output metadata."""
         if isinstance(mdata, Sequence):
             for mdata_i in mdata:
                 self.fix_meta_data(model, plot_variable, integrate, mdata_i)
             return
         unit = str(mdata.variable.unit)
         if model in self.cosmo_models:
-            if unit == "ng kg-1":
+            if unit in ["ng kg-1", "1e-12 Bq m-3"]:
                 new_unit = "Bq h m-3" if integrate else "Bq m-3"
-            elif unit == "1e-12 kg m-2":
+            elif unit in ["1e-12 kg m-2", "1e-12 Bq m-2"]:
                 # new_unit = "Bq h m-2" if integrate else "Bq m-2"
                 new_unit = "Bq m-2"
             # SR_TMP <
@@ -90,9 +92,9 @@ class FlexPartDataFixer:
                 log(wrn=msg)
                 return
         elif model in self.ifs_models:
-            if unit == "ng m-3":
+            if unit in ["ng m-3", "1e-12 Bq m-3"]:
                 new_unit = "Bq h m-3" if integrate else "Bq m-3"
-            elif unit == "1e-12 kg m-2":
+            elif unit in ["1e-12 kg m-2", "1e-12 Bq m-2"]:
                 # new_unit = "Bq h m-2" if integrate else "Bq m-2"
                 new_unit = "Bq m-2"
             # SR_TMP <