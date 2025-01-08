dcb17f54e780fee273eaa2685fb4ba8014ba613f
nestabur <6052881+nestabur@users.noreply.github.com>
2025-01-08 13:44:14 +0100

    Add comments clarifying how paths are calculated and how pdfs are merged (#63)
    * Add comments clarifying how paths are calculated and how pdfs are merged
    
    * Remove access to field since "formatted_file" is not an object
    
    Co-authored-by: Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>
    
    ---------
    
    Co-authored-by: Nestor Tarin Burriel <nestor.tarinburriel@meteoswiss.ch>
    Co-authored-by: Pirmin Kaufmann <pirmin.kaufmann@meteoswiss.ch>

--- src/pyflexplot/output.py
+++ src/pyflexplot/output.py
@@ -47,7 +47,13 @@ class FilePathFormatter:
             time_steps=time_steps,
             species_name=species_name,
         )
+
+        # we need to check whether the file path has already been formatted
+        # since the same path might be calculated, then we need to derive a unique path
+        # until is not in any of the previously formatted paths
         while path in self.previous:
+            # template and path are recalculated on each loop
+            # we need to first calculate the new template before re-formatting the file
             template = self.derive_unique_path(
                 self._format_template(
                     template,
