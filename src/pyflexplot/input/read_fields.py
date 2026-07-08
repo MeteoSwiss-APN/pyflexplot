34a5c5f1600fd5a0ebb4046df3b965633881c6f0
Paulina Grochal <paulina.grochal@meteoswiss.ch>
2026-07-08 12:20:00 +0200

    DT-392fix read fields model

--- src/pyflexplot/input/read_fields.py
+++ src/pyflexplot/input/read_fields.py
@@ -443,8 +443,8 @@ class InputFileEnsemble:
             # By default, sum up over all dimensions
             fld_time = merge_fields(read_fld_time_of_dimensions(timeless_panel_setup))
             fld_time = fld_time[..., np.newaxis]
-        if self.fixer and self.model_setup.name == "IFS-HRES":
-            # Note: IFS-HRES-EU is not global, so doesn't need this fix
+        if self.fixer and self.model_setup.name == "IFS-Global":
+            # Note: IFS-Europe is not global, so doesn't need this fix
             self.fixer.fix_global_grid(self.lon, fld_time, lon_axis=2)
         if self.config.add_ts0:
             fld_time = self._add_ts0_to_fld_time(fld_time)
